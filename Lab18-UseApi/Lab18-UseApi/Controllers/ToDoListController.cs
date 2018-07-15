983ef784bc6bfa72a5e1219f0545dc7414fe33ef
Max <gusstorm132@gmail.com>
2018-07-15 16:41:49 -0700

    scaffolding

---
+++ Lab18-UseApi/Lab18-UseApi/Controllers/ToDoListController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_UseApi.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab18-UseApi
+
+## Descrption
+This will be an MVC app that consumes a ToDo list api. It'll have browser/UI acess to the Create, Read all/one, Update by ID, and Delete item/empty list methods that apply to both lists and items.
+ 
